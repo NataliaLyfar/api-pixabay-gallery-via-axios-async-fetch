666f643f81e056bb6f74f4e47965ca8e20c9977b
Nataliia Lyfar <lifarnatala@gmail.com>
2022-05-08 11:15:39 +0300

    fixed fetch and started render gallery

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import './sass/main.scss';
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import "regenerator-runtime/runtime.js";
 
-import { fetchPixabay } from './js/fetchpixabay.js';
+import { fetchPics } from './js/fetchpixabay.js';
 import photoCardTpl from './partials/photocard.hbs';
 import { refs } from './js/refs.js';
 
@@ -11,63 +12,92 @@ const galleryLigthbox = new SimpleLightbox(".gallery a", {
     captionDelay: 250,
   });
 
-const initialData = {
-    query: null,
-    totalHits: 0,
-    page: 1,
-    hits: [],
-  };
-const dataCard = {
-    previewURL,
-    largeImageURL,
-    tags,
-    likes,
-    views,
-    comments,
-    downloads,
-  };
+// const initialData = {
+//     query: null,
+//     totalHits: 0,
+//     page: 1,
+//     hits: [],
+//   };
+// // const dataCard = {
+// //     previewURL,
+// //     largeImageURL,
+// //     tags,
+// //     likes,
+// //     views,
+// //     comments,
+// //     downloads,
+// //   };
 
-const renderCards = cards =>{
-    cards.map((...dataCard) => photoCardTpl(...dataCard)).join('');
-    galleryLigthbox.refresh();
+// const renderCards = dataCard =>{
+//     dataCard.map(({
+//       previewURL,
+//       largeImageURL,
+//       tags,
+//       likes,
+//       views,
+//       comments,
+//       downloads,
+//     }) => photoCardTpl(data)).join('');
+//     galleryLigthbox.refresh();
+// }
+  const renderPic = (pics) => pics.map((pic) => photoCardTpl(pic))
+const renderGallery = () => {
+  const markup = pics
+    .map(
+      (pic) => renderPic(pic)
+    )
+    .join("");
+ refs.gallery.innerHTML = markup;
 }
-// const renderCountryCard = (countries) => {
-//     const countriesQuantity = countries.length;
-//     if(countriesQuantity > 10){
-//         refs.countryInfo.innerHTML = '';
-//         refs.countryList.innerHTML = '';
-//         Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-//     }
-//     else if(countriesQuantity > 1 && countriesQuantity <= 10){
-//         refs.countryInfo.innerHTML = '';
-//         refs.countryList.innerHTML  = createCountryList(countries);
-//     } 
-//     else if(countriesQuantity === 1){
-//         refs.countryList.innerHTML = '';
-//         refs.countryInfo.innerHTML = countryCardTpl(countries[0]);
-//     }
-//         };
-// const onFetchError = () => {
-//     Notiflix.Notify.failure('Oops! There is no country with that name!');
-//     refs.countryInfo.innerHTML = '';
-//     refs.countryList.innerHTML = '';
-// };
-// const clearInput = () => refs.countryInfo.innerHTML = '';
 
-const onSearch = e => {
+
+
+// const renderGallery = async () => {
+//   const { query, page } = initialData;
+
+//   await fetchPixabay(query, page)
+//     .then((res) => {
+//       const { data } = res;
+
+//       if (data.hits.length) {
+//         if (page === 1) {
+//           initialData.totalHits = data.totalHits;
+//           initialData.hits = [];
+//           Notify.success(`Hooray! We found ${data.totalHits} images.`);
+//         }
+
+//         initialData.hits = initialData.hits.concat(data.hits);
+//         renderCards(data.hits);
+
+//         observer.observe(document.querySelector(".photo-card:last-child"));
+//       } else
+//         Notify.failure(
+//           "Sorry, there are no images matching your search query. Please try again."
+//         );
+//     })
+//     .catch((e) => console.error(e));
+// };
+
+const onFetchError = () => {
+  Notiflix.Notify.failure('Oops! There is no country with that name!');
+  // refs.countryInfo.innerHTML = '';
+  // refs.countryList.innerHTML = '';
+};
+const onSearch = async(e) => {
     e.preventDefault;
     const {
         elements: { searchQuery }
     } = e.currentTarget;
     initialData.query = searchQuery.value.toLowerCase().trim();
-    const {query} = initialData;
-    renderGallery();
-   
-//    if(query.length){
-//    refs.gallery.innerHTML = "";
-//     page = 1;
-//     renderGallery();
+    const { query } = initialData;
+    if (query.length) {
+      try {
+        const pics = await fetchPics(query);
+        renderGallery(pics);
+      } catch (error) {
+        onFetchError()
+      }
+    }
+}
+refs.searchForm.addEventListener('submit', onSearch);
 
-// }
-};
-refs.searchBtn.addEventListener('submit', onSearch);
--- src/js/fetchpixabay.js
+++ src/js/fetchpixabay.js
@@ -1,23 +1,34 @@
 import axios from "axios";
-
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '27183497-70bd3599297502793f5a9350a';
 const options = {
         "image_type": "photo",
         "orientation": "horizontal",
         "safesearch": true,
-        "per_page": 20,
-        
+        "per_page": 20, 
 }
-
-export const fetchPixabay = async (query) => {
-      await axios.get(BASE_URL,{
-          params: {
-            "key": API_KEY,
-            "q": query,
-            "page": 1,
-            ...options
-          } 
-      }).catch ((error) => console.error(error));
-    
+const initialData = {
+  query: null,
+  totalHits: 0,
+  page: 1,
+  hits: [],
+};
+export const fetchPics = async(query) => {
+  const { hits } = initialData;
+  const arrayOfPromises = hits.map(async (hit) => {
+  const response = await axios.get(BASE_URL,{
+      params: {
+        "key": API_KEY,
+        "q": query,
+        "page": page,
+        ...options
+      } 
+    });
+    return response.json();
+  });
+  page +=1;
+  const pics = await Promise.all(arrayOfPromises);
+  return pics;
   }
+  
+ 
